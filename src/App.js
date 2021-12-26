4a490513ae222e19bee83314f6caaad0b48eb4f4
Ola Råbius Magnusson <ola.piano@gmail.com>
2021-12-26 22:37:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>
+          <p>Create GitHub Repo</p>
+        </li>
+        <li>
+          <p>Create React App</p>
+        </li>
+        <li>
+          <p>Install gh-pages</p>
+          <code>npm install gh-pages --save-dev</code>
+        </li>
+        <li>
+          <p>Make changes in package.json</p>
+          <p>On top</p>
+          <code>"hompage": "https://username.github.io/repo"</code>
+          <p>In scripts</p>
+          <code>"predeploy": "npm run build"</code>
+          <code>"deploy": "gh-pages -d build"</code>
+        </li>
+        <li>
+          <p>Initialize git</p>
+          <code>git init</code>
+        </li>
+        <li>
+          <p>Add Remote</p>
+          <code>git remote add origin git@github.com:username/repo.git</code>
+        </li>
+        <li>
+          <p>Deploy</p>
+          <code>npm deploy</code>
+        </li>
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
